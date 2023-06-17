84318c5681c889cd47a8fd53668489afeafc8b55
Solodnikov <solodnikov@yandex.ru>
2023-06-17 16:01:36 +0300

    for 2 review

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -3,14 +3,15 @@ import scrapy
 from pep_parse.items import PepParseItem
 from pep_parse.settings import PEP_URL
 
+
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = [PEP_URL]
-    start_urls = ['https://' + PEP_URL +'/']
+    start_urls = ['https://' + PEP_URL + '/']
 
     def parse(self, response):
         all_pep = response.css(
-            'section[id=numerical-index] tbody tr td:nth-child(2) a::attr(href)'
+            'section[id=numerical-index] tr td:nth-child(2) a::attr(href)'
         )
         for pep_link in all_pep:
             yield response.follow(pep_link, callback=self.parse_pep)
